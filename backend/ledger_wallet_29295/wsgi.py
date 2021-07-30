d96542a71685ce6d335882c10cf840355c8252f7
Crowdbotics <team@crowdbotics.com>
2021-07-30 20:37:03 +0000

    Update models

--- backend/ledger_wallet_29295/wsgi.py
+++ backend/ledger_wallet_29295/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ledger_wallet_29295.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ledger_wallet_29295.settings")
 
 application = get_wsgi_application()
